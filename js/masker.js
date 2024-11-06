b850826593ee0926ac312677b72e54c00eb5e01d
jimmy.liu@homepartners.tech <jimmy.liu>
2024-11-06 15:24:04 +0800

    1

--- js/masker.js
+++ js/masker.js
@@ -17,35 +17,45 @@ uploadArea.ondrop = function (e) {
   uploadDragnDrop(e.dataTransfer.files[0]);
 };
 
+ var slider = document.getElementById('brushSize');
+
+  noUiSlider.create(slider, {
+    start: 30,
+	step: 5,
+    range: {
+      'min': 10,
+      'max': 100
+    },
+	pips: { 
+        mode: 'steps',
+        density: 5
+    }
+  });
+
+  slider.noUiSlider.on('update', function (values, handle) {
+	canvas.freeDrawingBrush.width = parseInt(values[handle]);
+  });
+
 canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
-canvas.freeDrawingBrush.width = 30;
-canvas.freeDrawingBrush.color = hexToRgb(
-  '#000000'
-);
+canvas.freeDrawingBrush.width = slider.noUiSlider.get();
+canvas.freeDrawingBrush.color = hexToRgb('#000000');
 fabric.textureSize = 4096;
 
 $("html").on("paste", function (event) {
-  if (
-    event.target.id === "customMaskURL" ||
-    event.target.id === "saveFromURLURL" ||
-    event.target.id == "copyToClipboard"
-  ) {
-  } else {
-    if (event.originalEvent.clipboardData) {
-      var items = event.originalEvent.clipboardData.items;
-      if (items) {
-        for (index in items) {
-          var item = items[index];
-          if (item.kind === "file") {
-            var blob = item.getAsFile();
-            var source = URL.createObjectURL(blob);
-            loadSourceImage(source, false);
-            return;
-          }
-        }
-      }
-    }
-  }
+	if (event.originalEvent.clipboardData) {
+	  var items = event.originalEvent.clipboardData.items;
+	  if (items) {
+		for (index in items) {
+		  var item = items[index];
+		  if (item.kind === "file") {
+			var blob = item.getAsFile();
+			var source = URL.createObjectURL(blob);
+			loadSourceImage(source, false);
+			return;
+		  }
+		}
+	  }
+	}
 });
 
 function uploadImage(e) {
@@ -65,9 +75,9 @@ function uploadDragnDrop(file) {
 
 function loadSourceImage(baseUrl, externalImage) {
   var resizeFactor = 1;
-  var minWidth = 300; // 设置最小宽度
-  var minHeight = 300; // 设置最小高度
-  var margin = 50; // 设置边距
+  var minWidth = 300; 
+  var minHeight = 300; 
+  var margin = 50; 
 
   sourceImageUrl = baseUrl;
   fabric.Image.fromURL(sourceImageUrl, function (img) {
@@ -126,9 +136,7 @@ function loadSourceImage(baseUrl, externalImage) {
 }
 
 
-$(document).on("input", "#brushSize", function () {
-  canvas.freeDrawingBrush.width = parseInt($(this).val());
-});
+
 
 function undo() {
   if (canvas._objects.length > 0) {
@@ -154,88 +162,6 @@ var opac = 1;
 
 $(document).on("keydown", function (e) {
   var target = $(e.target);
-  if (e.which == 37) {
-    if (target.is("input")) {
-    } else {
-      e.preventDefault();
-      if (document.getElementById("savedRounds").style.display == "block") {
-        displaySavedRounds(1);
-      } else {
-        var originalAngle = maskImage.get("angle");
-        maskImage.rotate(originalAngle - 2);
-        canvas.renderAll();
-      }
-    }
-  }
-  if (e.which == 39) {
-    if (target.is("input")) {
-    } else {
-      e.preventDefault();
-      if (document.getElementById("savedRounds").style.display == "block") {
-        displaySavedRounds(2);
-      } else {
-        var originalAngle = maskImage.get("angle");
-        maskImage.rotate(originalAngle + 2);
-        canvas.renderAll();
-      }
-    }
-  }
-  if (e.which == 40) {
-    e.preventDefault();
-    if (canvas.getActiveObject()) {
-      var obj = canvas.getActiveObject();
-    } else {
-      var obj = canvas._objects[canvas._objects.length - 1];
-    }
-    if (opac > 0.1) {
-      opac = opac - 0.1;
-    }
-    obj.set("opacity", opac);
-    canvas.renderAll();
-  }
-  if (e.which == 38) {
-    e.preventDefault();
-    if (canvas.getActiveObject()) {
-      var obj = canvas.getActiveObject();
-    } else {
-      var obj = canvas._objects[canvas._objects.length - 1];
-    }
-    if (opac <= 1) {
-      opac = opac + 0.1;
-    }
-    obj.set("opacity", opac);
-    canvas.renderAll();
-  }
-  if (e.which == 220) {
-    if (canvas.getActiveObject()) {
-      var obj = canvas.getActiveObject();
-    } else {
-      var obj = canvas._objects[canvas._objects.length - 1];
-    }
-    var object = fabric.util.object.clone(obj);
-    object.set("top", object.top + 7);
-    object.set("left", object.left + 7);
-    canvas.add(object);
-  }
-  if (e.which === 45) {
-    var newSubInput = document.getElementById("newSubInput");
-    newSubInput.style.display = "inline-block";
-    var newSub = newSubInput.value;
-    var newPost = document.getElementById("PostReddit");
-    newPost.innerText = "Post to /r/";
-  }
-  if (e.which === 87) {
-    canvas.freeDrawingBrush.width =
-      parseInt(document.getElementById("brushSize").value) + 5;
-    document.getElementById("brushSize").value =
-      parseInt(document.getElementById("brushSize").value) + 5;
-  }
-  if (e.which === 83) {
-    canvas.freeDrawingBrush.width =
-      parseInt(document.getElementById("brushSize").value) - 5;
-    document.getElementById("brushSize").value =
-      parseInt(document.getElementById("brushSize").value) - 5;
-  }
   //bind delete object to DEL key
   if (e.which === 46) {
     deleteObject();
@@ -263,18 +189,16 @@ function hexToRgb(hex) {
 function eraser() {
   canvas.freeDrawingBrush = new fabric.EraserBrush(canvas);
   canvas.isDrawingMode = true;
-  canvas.freeDrawingBrush.width = parseInt(
-    document.getElementById("brushSize").value
-  );
+  var brushSize = slider.noUiSlider.get();
+  canvas.freeDrawingBrush.width = parseInt(brushSize);
   canvas.renderAll();
 }
 //function to use the brush
 function brush() {
   canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
   canvas.isDrawingMode = true;
-  canvas.freeDrawingBrush.width = parseInt(
-    document.getElementById("brushSize").value
-  );
+  var brushSize = slider.noUiSlider.get();
+  canvas.freeDrawingBrush.width = parseInt(brushSize);
   canvas.freeDrawingBrush.color = hexToRgb(
     '#000000'
   );
@@ -312,19 +236,19 @@ function deleteObject() {
 
 function downloadMask() {
 	  var hasMask = false;
-  // 创建一个临时画布
+  
   var tempCanvas = document.createElement('canvas');
   tempCanvas.width = canvas.width;
   tempCanvas.height = canvas.height;
   var tempContext = tempCanvas.getContext('2d');
 
-  // 确保背景透明
+  
   tempContext.fillStyle = 'rgba(0, 0, 0, 0)';
   tempContext.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
 
-  // 绘制对象（排除背景）
+  
   canvas.forEachObject(function(obj) {
-    if (!obj.excludeFromExport) { // 假设有一个属性用于标记不导出的对象
+    if (!obj.excludeFromExport) { 
 	  hasMask = true;
       obj.clone(function(clonedObj) {
         clonedObj.render(tempContext);
@@ -337,7 +261,7 @@ function downloadMask() {
     return;
   }
 
-  // 转换为数据 URL 并创建下载链接
+  
   var dataURL = tempCanvas.toDataURL('image/png');
   var downloadLink = document.createElement('a');
   downloadLink.href = dataURL;
